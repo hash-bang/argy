ea57d916d1f0c07db531df0a3a87b274e7eed474
Matt Carter <m@ttcarter.com>
2016-09-27 15:29:26 +1000

    More compatibility tests

--- test/compatibility.js
+++ test/compatibility.js
@@ -22,4 +22,40 @@ describe('argy - compatibility tests', function() {
 		}('one', 'two', 'three');
 	});
 
+
+	/**
+	* Even though the `arguments` object is still present and a,b,c is specified as parameters, setting values outside the maximum argument index doesnt work
+	*/
+	it('should NOT be able to override args if the function specifies them', function() {
+		var result = function myFunc(a, b, c) {
+
+			var dummyFunc = function(args) {
+				args[0] = 'foo';
+				args[1] = 'bar';
+				args[2] = 'baz';
+			}(arguments);
+
+			expect(a).to.equal('foo');
+			expect(b).to.be.undefined;
+			expect(c).to.be.undefined;
+
+		}('one');
+	});
+
+	it('should NOT be able to override args if the function specifies them (via arguments.callee.arguments)', function() {
+		var result = function myFunc(a, b, c) {
+
+			var dummyFunc = function(args) {
+				args.callee.arguments[0] = 'foo';
+				args.callee.arguments[1] = 'bar';
+				args.callee.arguments[2] = 'baz';
+			}(arguments);
+
+			expect(a).to.equal('foo');
+			expect(b).to.be.undefined;
+			expect(c).to.be.undefined;
+
+		}('one');
+	});
+
 });
