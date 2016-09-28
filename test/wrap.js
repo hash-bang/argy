88fe47b1d0f42690d3a5dff2be558fc6d74f26cb
Matt Carter <m@ttcarter.com>
2016-09-28 14:34:33 +1000

    Split wrap tests into as + builder syntaxes

--- test/wrap.js
+++ test/wrap.js
@@ -1,9 +1,9 @@
 var argy = require('..');
 var expect = require('chai').expect;
 
-describe('argy.wrap()', function() {
+describe('argy.wrap() test #1', function() {
 
-	it('should process basic scenarios #1', function() {
+	it('should process a basic scenario using "as" syntax', function() {
 		var identify = argy().as('[string] [number]').wrap(function(name, age) {
 			return [name, age];
 		});
@@ -14,10 +14,42 @@ describe('argy.wrap()', function() {
 		expect(identify()).to.deep.equal([undefined, undefined]);
 	});
 
-	it.skip('should process basic scenarios #2', function() {
+	it('should process basic scenario using builder syntax', function() {
+		var identify = argy()
+			.optional('string')
+			.optional('number')
+			.wrap(function(name, age) {
+				return [name, age];
+			});
+
+		expect(identify('John')).to.deep.equal(['John', undefined]);
+		expect(identify('Matt', 30)).to.deep.equal(['Matt', 30]);
+		expect(identify(23)).to.deep.equal([undefined, 23]);
+		expect(identify()).to.deep.equal([undefined, undefined]);
+	});
+
+});
+
+describe('argy.wrap() test #2', function() {
+
+	it('should process basic scenario using "as" syntax', function() {
 		var petLister = argy().as('[string] [string|array]').wrap(function(name, pets) {
 			return [name, pets];
 		});
+
+		expect(petLister('Joe')).to.deep.equal(['Joe', []]);
+		expect(petLister('Sally', 'Felix')).to.deep.equal(['Sally', ['Felix']]);
+		expect(petLister('Joan', ['Glitch', 'Widget'])).to.deep.equal(['Joan', ['Glitch', 'Widget']]);
+		expect(petLister(['Rover', 'Rex'])).to.equal([undefined, ['Rover', 'Rex']]);
+	});
+
+	it('should process basic scenario using builder syntax', function() {
+		var petLister = argy()
+			.optional('string')
+			.optional(['string', 'array'])
+			.wrap(function(name, pets) {
+				return [name, pets];
+			});
 
 		expect(petLister('Joe')).to.deep.equal(['Joe', []]);
 		expect(petLister('Sally', 'Felix')).to.deep.equal(['Sally', ['Felix']]);
