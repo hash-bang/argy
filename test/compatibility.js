57c8617a2c86b1fa1e5dadeb27f38385deacc488
Matt Carter <m@ttcarter.com>
2016-09-28 11:34:50 +1000

    More compatibility tests

--- test/compatibility.js
+++ test/compatibility.js
@@ -1,10 +1,11 @@
 var expect = require('chai').expect;
 
-describe('argy - compatibility tests', function() {
+describe('argy - compatibility tests (arguments object)', function() {
 
 	/**
 	* Verifies that a sub function can be passed the arguments object which can in turn override the values from within the function
 	* This is similar to setting object keys of a passed object
+	* NOTE: This only works when 'use strict' is NOT present
 	*/
 	it('should be able to override args if the function specifies them', function() {
 		var result = function myFunc(a, b, c) {
@@ -25,6 +26,7 @@ describe('argy - compatibility tests', function() {
 
 	/**
 	* Even though the `arguments` object is still present and a,b,c is specified as parameters, setting values outside the maximum argument index doesnt work
+	* NOTE: This only works when 'use strict' is NOT present
 	*/
 	it('should NOT be able to override args if the function specifies them', function() {
 		var result = function myFunc(a, b, c) {
@@ -42,6 +44,10 @@ describe('argy - compatibility tests', function() {
 		}('one');
 	});
 
+	/**
+	* Same as above but accessing the arguments object via the callee object
+	* NOTE: This only works when 'use strict' is NOT present
+	*/
 	it('should NOT be able to override args if the function specifies them (via arguments.callee.arguments)', function() {
 		var result = function myFunc(a, b, c) {
 
@@ -56,6 +62,27 @@ describe('argy - compatibility tests', function() {
 			expect(c).to.be.undefined;
 
 		}('one');
+	});
+
+});
+
+
+describe('argy - compatibility tests (passing by reference)', function() {
+
+	it('should NOT be able to override args if the function specifies them', function() {
+		var result = function myFunc(a, b, c) {
+
+			var dummyFunc = function(alpha, beta, gamma) {
+				alpha = 'foo';
+				beta = 'bar';
+				gamma = 'baz';
+			}(a, b, c);
+
+			expect(a).to.equal('one');
+			expect(b).to.equal('two');
+			expect(c).to.equal('three');
+
+		}('one', 'two', 'three');
 	});
 
 });
